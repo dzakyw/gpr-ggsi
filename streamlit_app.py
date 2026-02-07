b3d4990b549003ff63683ee7bb6ff0b7484f2e8d
dzakyw <96814015+dzakyw@users.noreply.github.com>
2026-02-07 13:43:23 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1796,7 +1796,8 @@ if st.session_state.data_loaded:
             
             # Use pcolormesh for elevation-adjusted display
             mesh = ax_elev.pcolormesh(X, Y_elev, st.session_state.processed_array,
-                                     cmap='seismic', shading='auto', alpha=0.9)
+                                      vmin=vmin_plot, vmax=vmax_plot,
+                                      cmap='seismic', shading='auto', alpha=0.9)
             
             ax_elev.set_xlabel('Distance along profile (m)')
             ax_elev.set_ylabel('Elevation (m)')
@@ -2242,5 +2243,6 @@ st.markdown(
     "</div>",
     unsafe_allow_html=True
 )
+
 
 
