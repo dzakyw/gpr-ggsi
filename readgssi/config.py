806d05421bab721484f5977934a34dba8b29d3fa
dzakyw <96814015+dzakyw@users.noreply.github.com>
2026-02-17 00:24:43 +0700

    Update config.py

--- readgssi/config.py
+++ readgssi/config.py
@@ -1,12 +1,9 @@
 from datetime import datetime
-from readgssi.__init__ import __version__, name
 import readgssi.plot as plot
 
-"""
-This module contains some things readgssi needs to operate, both command line and python-related.
-"""
-dist = None
+name = "readgssi"
 __version__ = "local"
+
 year = datetime.now().year
 author = 'Ian Nesbitt'
 affil = 'School of Earth and Climate Sciences, University of Maine'
